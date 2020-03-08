cb307f3534af213d951ba2656bbc43e65df499fe
LeoKnox <LeoKnox@users.noreply.github.com>
2020-03-07 17:33:39 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -7,12 +7,13 @@ def index():
     return render_template("index.html", index=True)
 
 @app.route("/dungeon")
-def dungeon():
+@app.route("/dungeon/<dungeonName>")
+def dungeon(dungeonName="Generic"):
     dungeonData = [{"dungeonID":"1111","name":"Entry","length":"4","width":"4","material":"stone"},
     {"dungeonID":"1112","name":"Storage","length":"6","width":"5","material":"wood"},
     {"dungeonID":"1113","name":"Tome","length":"5","width":"8","material":"stone"}]
     print(dungeonData)
-    return render_template("dungeon.html", dungeonData=dungeonData, dungeon=True)
+    return render_template("dungeon.html", dungeonData=dungeonData, dungeon=True, dungeonName = dungeonName)
 
 @app.route("/create")
 def create():
