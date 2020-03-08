502a167b4b1bd665d51a5d110454ddb0b662da75
LeoKnox <LeoKnox@users.noreply.github.com>
2020-03-08 12:29:12 -0700

    Add files via upload

---
+++ application/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    monster_id  =   StringField("Monster Id", validators=[DataRequired()])
+    called      =   StringField("Called", validators=[DataRequired()])
+    remember_me =   BooleanField("Remember Me")
+    submit      =   SubmitField("Login")
+
+class PopulateForm(FlaskForm):
+    monster_id  =   IntegerField("Monster Id", validators=[DataRequired()])
+    called      =   StringField("Called", validators=[DataRequired()])
+    monster_type=   StringField("Type", validators=[DataRequired()])
+    damage      =   IntegerField("Damage", validators=[DataRequired()])
+    submit      =   SubmitField("Login")
